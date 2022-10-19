160eef3fc4b59b5d7442b14aa3bb3272d160ab29
Istiak Ahmed <istiakahmed104127@gmail.com>
2022-10-19 22:22:36 +0600

    some changes

--- src/components/Login-Signup-Component/SignupComponent.jsx
+++ src/components/Login-Signup-Component/SignupComponent.jsx
@@ -46,7 +46,7 @@ const SignupComponent = () => {
             />
           </div>
 
-          <button class="mt-5 tracking-wide font-semibold bg-[#E7303E] text-gray-100 w-full py-4 rounded-lg hover:bg-[#eb0619] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+          <button class="tracking-wide font-semibold bg-[#E7303E] text-gray-100 w-full py-4 rounded-lg hover:bg-[#eb0619] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
             <svg
               class="w-6 h-6 -ml-2"
               fill="none"
