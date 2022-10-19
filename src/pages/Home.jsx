e441f14896afd13b1a7d31c13e672ffff984d739
Mostafizur rahman <sourav10681@gmail.com>
2022-10-19 18:05:40 +0600

    some components design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Master from '../components/Home/Master';
+import Mock from '../components/Home/Mock';
+import Practice from '../components/Home/Practice';
+import Review from '../components/Home/Review';
 
 const Home = () => {
     return (
         <div>
             <Master></Master>
+            <Mock></Mock>
+            <Practice></Practice>
+            <Review></Review>
         </div>
     );
 };
