f33b13388c7d29cfd63a0c568f79fb4650aac5eb
Istiak Ahmed <istiakahmed104127@gmail.com>
2022-10-26 00:20:37 +0600

    conflict error solved

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -23,81 +23,56 @@ const Navbar = () => {
       <div className="lg:flex items-center justify-between bg-white py-4 lg:px-10 px-7">
         <div>
           <img src={logo} width="100px" height="100px" alt={logo} />
+
           <div
             onClick={() => setOpen(!open)}
             className="text-4xl absolute right-8 top-6 text-red-600 cursor-pointer lg:hidden"
           >
             <ion-icon name={open ? "close" : "menu"}></ion-icon>
           </div>
+        </div>
 
-          <ul
-            className={`lg:flex lg:items-center md:pb-0 pb-7 absolute lg:static bg-red-600 lg:bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-4 transition-all duration-500 ease-in ${
-              open ? "top-20 " : "top-[-490px]"
-            }`}
-          >
-            {Links.map((link) => (
-              <li
-                key={link.name}
-                className="ml-3 lg:ml-5 font-bold text-lg lg:my-0 my-7"
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-4xl absolute right-8 top-6 text-red-600 cursor-pointer lg:hidden"
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
+        </div>
+
+        <ul
+          className={`lg:flex lg:items-center md:pb-0 pb-7 absolute lg:static bg-red-600 lg:bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-4 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li
+              key={link.name}
+              className="ml-3 lg:ml-5 font-bold text-lg lg:my-0 my-7"
+            >
+              <a
+                href={link.link}
+                className="text-white font-sans lg:text-black lg:hover:text-red-600 hover:text-2xl duration-500"
               >
-                <a
-                  href={link.link}
-                  className="text-white font-sans lg:text-[#000f38] lg:hover:text-red-600 hover:text-2xl duration-500"
-                >
-                  {link.name}
-                </a>
-              </li>
-            ))}
-
-            <Link to="/signup">
+                {link.name}
+              </a>
+            </li>
+          ))}
+          {user ? (
+            <button
+              onClick={() => logout()}
+              className="cursor-pointer font-sans lg:mr-2 lg:ml-4 mb-5 lg:mb-0 border-2 lg:border-0 lg:hover:border-2  text-white hover:text-black font-bold text-lg  px-3 py-2 rounded-lg bg-red-600 hover:border-red-600 hover:bg-white duration-500"
+            >
+              logout
+            </button>
+          ) : (
+            <Link to="/login">
               <button className="cursor-pointer font-sans lg:mr-2 lg:ml-4 mb-5 lg:mb-0 border-2 lg:border-0 lg:hover:border-2  text-white hover:text-black font-bold text-lg  px-3 py-2 rounded-lg bg-red-600 hover:border-red-600 hover:bg-white duration-500">
-                Sign Up
+                Login
               </button>
             </Link>
-          </ul>
-        </div>
+          )}
+        </ul>
       </div>
-
-      <div
-        onClick={() => setOpen(!open)}
-        className="text-4xl absolute right-8 top-6 text-red-600 cursor-pointer lg:hidden"
-      >
-        <ion-icon name={open ? "close" : "menu"}></ion-icon>
-      </div>
-
-      <ul
-        className={`lg:flex lg:items-center md:pb-0 pb-7 absolute lg:static bg-red-600 lg:bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-4 transition-all duration-500 ease-in ${
-          open ? "top-20 " : "top-[-490px]"
-        }`}
-      >
-        {Links.map((link) => (
-          <li
-            key={link.name}
-            className="ml-3 lg:ml-5 font-bold text-lg lg:my-0 my-7"
-          >
-            <a
-              href={link.link}
-              className="text-white font-sans lg:text-black lg:hover:text-red-600 hover:text-2xl duration-500"
-            >
-              {link.name}
-            </a>
-          </li>
-        ))}
-        {user ? (
-          <button
-            onClick={() => logout()}
-            className="cursor-pointer font-sans lg:mr-2 lg:ml-4 mb-5 lg:mb-0 border-2 lg:border-0 lg:hover:border-2  text-white hover:text-black font-bold text-lg  px-3 py-2 rounded-lg bg-red-600 hover:border-red-600 hover:bg-white duration-500"
-          >
-            logout
-          </button>
-        ) : (
-          <Link to="/login">
-            <button className="cursor-pointer font-sans lg:mr-2 lg:ml-4 mb-5 lg:mb-0 border-2 lg:border-0 lg:hover:border-2  text-white hover:text-black font-bold text-lg  px-3 py-2 rounded-lg bg-red-600 hover:border-red-600 hover:bg-white duration-500">
-              Login
-            </button>
-          </Link>
-        )}
-      </ul>
     </div>
   );
 };
