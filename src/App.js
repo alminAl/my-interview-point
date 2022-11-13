e66ff25fe79547785e6f8bb65397d274580fd1d5
Istiak Ahmed <istiakahmed104127@gmail.com>
2022-11-13 16:53:31 +0600

    full support page done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ import Contact from "./pages/Contact";
 import SideNavBarPlayGround from "./pages/playground/SideNavBarPlayGround";
 import SupportPage from "./pages/SupportPage";
 import DeleteAccount from "./components/Support/Article/DeleteAccount";
+import CreateAccount from "./components/Support/Article/CreateAccount";
+import ActivateAccount from "./components/Support/Article/ActivateAccount";
+import OrganizationAccount from "./components/Support/Article/OrganizationAccount";
+import Requirement from "./components/Support/Article/Requirement";
+import Resolve from "./components/Support/Article/Resolve";
+import Manage from "./components/Support/Article/Manage";
 
 
 
@@ -44,6 +50,12 @@ function App() {
             <Route path="/blog" element={<Blog></Blog>}></Route>
             <Route path="/support" element={<SupportPage/>}></Route>
             <Route path="/support/deleteAccount" element={<DeleteAccount/>}></Route>
+            <Route path="/support/createAccount" element={<CreateAccount/>}></Route>
+            <Route path="/support/activateAccount" element={<ActivateAccount/>}></Route>
+            <Route path="/support/organizationAccount" element={<OrganizationAccount/>}></Route>
+            <Route path="/support/requirement" element={<Requirement/>}></Route>
+            <Route path="/support/resolve" element={<Resolve/>}></Route>
+            <Route path="/support/manage" element={<Manage/>}></Route>
             <Route path="/contact" element={<Contact></Contact>}></Route>
             {/* playground link */}
             <Route path="/playground/drawer" element={<SideNavBarPlayGround />}></Route>
--- src/components/Support/SupportQuestions.jsx
+++ src/components/Support/SupportQuestions.jsx
@@ -19,54 +19,54 @@ const SupportQuestions = () => {
             <h6 className="text-lg lg:text-xl text-center lg:text-left mt-3 mb-2 font-bold text-[#000f38]">
               Create an account
             </h6>
-            <p className="text-base text-center lg:text-left text-red-600 ">
+           <Link to="createAccount"> <p className="text-base text-center lg:text-left text-red-600 ">
               Read the Article
-            </p>
+            </p></Link>
           </div>
           <div className="mb-8 lg:mb-0 mx-auto w-full lg:mx-0 ">
             <img src={activate} alt={activate} className="mx-auto lg:mx-0" />
             <h6 className="text-lg text-center lg:text-left lg:text-xl mt-3 mb-2 font-bold text-[#000f38]">
               Activate my account
             </h6>
-            <p className="text-base text-center lg:text-left text-red-600 ">
+            <Link to="activateAccount"> <p className="text-base text-center lg:text-left text-red-600 ">
               Read the Article
-            </p>
+            </p></Link>
           </div>
           <div className="mb-8 lg:mb-0 mx-auto w-full lg:mx-0 ">
             <img src={setup} alt={setup} className="mx-auto lg:mx-0" />
             <h6 className="text-lg text-center lg:text-left lg:text-xl mt-3 mb-2 font-bold text-[#000f38]">
               Setup an account through your organization
             </h6>
-            <p className="text-base text-center lg:text-left text-red-600 ">
+            <Link to="organizationAccount"> <p className="text-base text-center lg:text-left text-red-600 ">
               Read the Article
-            </p>
+            </p></Link>
           </div>
           <div className="mb-8 lg:mb-0 mx-auto w-full lg:mx-0 ">
             <img src={system} alt={system} className="mx-auto lg:mx-0" />
             <h6 className="text-lg text-center lg:text-left lg:text-xl mt-3 mb-2 font-bold text-[#000f38]">
               System requirements
             </h6>
-            <p className="text-base text-center lg:text-left text-red-600 ">
+            <Link to="requirement"> <p className="text-base text-center lg:text-left text-red-600 ">
               Read the Article
-            </p>
+            </p></Link>
           </div>
           <div className="mb-8 lg:mb-0 mx-auto w-full lg:mx-0 ">
             <img src={resolves} alt={resolves} className="mx-auto lg:mx-0" />
             <h6 className="text-lg text-center lg:text-left lg:text-xl mt-3 mb-2 font-bold text-[#000f38]">
               Resolve issues with microphone/webcam
             </h6>
-            <p className="text-base text-center lg:text-left text-red-600 ">
+            <Link to="resolve"> <p className="text-base text-center lg:text-left text-red-600 ">
               Read the Article
-            </p>
+            </p></Link>
           </div>
           <div className="mb-8 lg:mb-0 mx-auto w-full lg:mx-0 ">
             <img src={manage} alt={manage} className="mx-auto lg:mx-0" />
             <h6 className="text-lg text-center lg:text-left lg:text-xl mt-3 mb-2 font-bold text-[#000f38]">
               Manage your settings
             </h6>
-            <p className="text-base text-center lg:text-left text-red-600 ">
+            <Link to="manage"> <p className="text-base text-center lg:text-left text-red-600 ">
               Read the Article
-            </p>
+            </p></Link>
           </div>
           
         </div>
