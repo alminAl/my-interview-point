50bb2f3a09a9cd9ef89d3b8af61f1589d4a552e4
Md. Riajul Islam <riaj17021995@gmail.com>
2022-11-16 15:36:59 +0600

    quiz issues resolved

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,85 +1,49 @@
 import React from "react";
-// import { useEffect, useState } from "react";
+import quizzes from "../JsonData/quizJSON";
 
 const Quiz = () => {
-  //   const [quizzes, setQuiz] = useState([]);
-
-  //   useEffect(() => {
-  //     const fetcher = async () => {
-  //       const res = await fetch("../JsonData/quizJSON.json");
-  //       const data = await res.json();
-  //       setQuiz(data);
-  //     };
-  //     fetcher();
-  //   }, []);
-  //   console.log(quizzes);
-
   return (
     <div className="min-h-full md:mx-36 mt-10 lg:mb-28 mb-5 bg-zinc-200">
       <h1 className="text-5xl text-center font-bold">Quiz</h1>
 
-      <form onSubmit="">
-        <div className="my-3 p-5">
-          <h1 className="bg-zinc-400 rounded p-1">1. What is ReactJS?</h1>
-          <div className="px-8">
-            <input type="radio" />
-            <label className="px-2">A. Server Side Framework</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">B. User Interface Framework</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">C. Both A & B</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">D. None of the above</label>
-          </div>
-        </div>
+      <form>
+        {quizzes.map((item) => {
+          const {
+            id,
+            question,
+            option_A,
+            option_B,
+            option_C,
+            option_D,
+            correctAns,
+          } = item;
 
-        <div className="my-3 p-5">
-          <h1 className="bg-zinc-400 rounded p-1">
-            2. Which of the following is used in React.js to increase
-            performance?
-          </h1>
-          <div className="px-8">
-            <input type="radio" />
-            <label className="px-2">A. Virtual DOM</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">B. Original DOM</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">C. Both A & B</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">D. None of the above</label>
-          </div>
-        </div>
-
-        <div className="my-3 p-5">
-          <h1 className="bg-zinc-400 rounded p-1">
-            3. Identify the one which is used to pass data to components from
-            outside?
-          </h1>
-          <div className="px-8">
-            <input type="radio" />
-            <label className="px-2">A. Render with arguments</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">B. setState</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">C. props</label>
-            <br />
-            <input type="radio" />
-            <label className="px-2">D. PropTypes</label>
-          </div>
-        </div>
+          return (
+            <div className="my-3 p-5" key={id}>
+              <h1 className="bg-zinc-400 rounded p-1">{question}</h1>
+              <div className="px-8">
+                <input type="radio" />
+                <label className="px-2">A. {option_A}</label>
+                <br />
+                <input type="radio" />
+                <label className="px-2">B. {option_B}</label>
+                <br />
+                <input type="radio" />
+                <label className="px-2">C. {option_C}</label>
+                <br />
+                <input type="radio" />
+                <label className="px-2">D. {option_D}</label>
+                <br />
+              </div>
+            </div>
+          );
+        })}
 
         <div className="px-8 text-end pb-5">
-        <button className="rounded-full bg-violet-900 px-5 py-3 text-white font-semibold hover:bg-violet-800">Submit</button>
+          <button className="rounded-full bg-violet-900 px-5 py-3 text-white font-semibold hover:bg-violet-800">
+            Submit
+          </button>
         </div>
-        
       </form>
     </div>
   );
